330265f56781168c90741c8d305718ddaf03b18b
Rose Cass <rose.cass12@gmail.com>
2017-04-22 15:44:47 -0700

    Updated the last line with the total score

--- src/components/ScoreBox/TotalScore.js
+++ src/components/ScoreBox/TotalScore.js
@@ -1,12 +1,14 @@
 import React from 'react';
 const TotalScore = props => {
   // TODO: 2) Destructure TotalScore props
+  const {totalScore} = props
+
   return (
     <div className="total-score">
       {/* TODO: 2) Render label */}
       <h2>{'TOTAL SCORE'}</h2>
       {/* TODO: 3) Render totalScore */}
-      <span>{13}</span>
+      <span>{totalScore}</span>
     </div>
   );
 };
